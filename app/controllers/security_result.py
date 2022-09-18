5d19c75c8c57f3c4cff06b338834cdbe4eede006
Ubuntu <ubuntu@ip-172-31-43-125.ap-northeast-2.compute.internal>
2022-09-18 16:19:36 +0000

    feat: createSecurityResult api 생성 및 security_results db column 추가

--- app/controllers/security_result.py
+++ app/controllers/security_result.py
@@ -1,13 +1,22 @@
 from flask import Blueprint, request, jsonify
+
+from ..utils.db import db_apply
 from ..models import db
-from ..models.security_result import SecurityResult
+from ..models.security_result import *
 
 bp = Blueprint('security_result', __name__, url_prefix='/api/v1/security_result')
 
 @bp.route('/createSecurityResult', methods=['POST'])
-def create_user():
-    result = SecurityResult(1, 1, '~/securityfiles/hoho')
+def create_security_result():
+    try:
+        params = request.get_json()
+        if(not params['pipeline_id'] or not params['pipeline_name'] or not params['user_id'] or not params['resultfile_path']):
+            return "check your values"
 
-    db.session.add(result)
-    db.session.commit()
-    return "complict"
+        result = SecurityResult(params['pipeline_id'], params['pipeline_name'], params['user_id'], params['resultfile_path'])
+        db_apply([result])
+        
+        return "complict"
+    except Exception as e:
+        print(e)
+        return "create security result faild"
--- app/models/security_result.py
+++ app/models/security_result.py
@@ -7,20 +7,22 @@ class SecurityResult(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     pipeline_id = db.Column(db.Integer, db.ForeignKey('pipelines.id'), nullable=False)
+    pipeline_name = db.Column(db.String(100, 'utf8mb4_unicode_ci'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) # 실행시킨 사람
     resultfile_path = db.Column(db.String(1024, 'utf8mb4_unicode_ci'), nullable=False)
     createAt = db.Column(db.DateTime, default=datetime.utcnow())
     updateAt = db.Column(db.DateTime, default=datetime.utcnow())
     deleteAt = db.Column(db.DateTime)
 
-    def __init__(self, pipeline_id, user_id, resultfile_path):
+    def __init__(self, pipeline_id, pipeline_name, user_id, resultfile_path):
         self.pipeline_id = pipeline_id
+        self.pipeline_name = pipeline_name
         self.user_id = user_id
         self.resultfile_path = resultfile_path
 
 class SecurityResultSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'pipeline_id', 'user_id', 'resultfile_path', 'createAt', 'updateAt', 'deleteAt')
+        fields = ('id', 'pipeline_id', 'pipeline_name', 'user_id', 'resultfile_path', 'createAt', 'updateAt', 'deleteAt')
 
 security_result_schema = SecurityResultSchema()
 security_results_schema = SecurityResultSchema(many=True)
