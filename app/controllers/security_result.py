b08bf06bf5f9b83ed99b7def8dbe462c789c9adf
Ubuntu <ubuntu@ip-172-31-43-125.ap-northeast-2.compute.internal>
2022-09-19 16:01:36 +0000

    fix: pipelines table과 security_results table에서  column이 서로 바뀌어 있는 것 수정

--- app/controllers/security_result.py
+++ app/controllers/security_result.py
@@ -11,10 +11,10 @@ bp = Blueprint('security_result', __name__, url_prefix='/api/v1/security_result'
 def create_security_result():
     try:
         params = request.get_json()
-        if(not params['pipeline_id'] or not params['pipeline_name'] or not params['user_id'] or not params['resultfile_path']):
+        if(not params['pipeline_id'] or not params['user_id'] or not params['resultfile_path']):
             return resp(400, "check your values")
 
-        result = SecurityResult(params['pipeline_id'], params['pipeline_name'], params['user_id'], params['resultfile_path'])
+        result = SecurityResult(params['pipeline_id'], params['user_id'], params['resultfile_path'])
         db_apply([result])
         
         return resp(201, "create security result success")
--- app/models/pipeline.py
+++ app/models/pipeline.py
@@ -6,6 +6,7 @@ class Pipeline(db.Model):
     __tablename__ = "pipelines"
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    pipeline_name = db.Column(db.String(100, 'utf8mb4_unicode_ci'), nullable=False)
     repo_url = db.Column(db.String(1024, 'utf8mb4_unicode_ci'), nullable=False)
     jenkinsfile_path = db.Column(db.String(1024, 'utf8mb4_unicode_ci'), nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
@@ -16,14 +17,15 @@ class Pipeline(db.Model):
     # relationship
     security_result = relationship('SecurityResult')
     
-    def __init__(self, repo_url, jenkinsfile_path, owner_id):
+    def __init__(self, pipeline_name, repo_url, jenkinsfile_path, owner_id):
+        self.pipeline_name = pipeline_name
         self.repo_url = repo_url
         self.jenkinsfile_path = jenkinsfile_path
         self.owner_id = owner_id
 
 class PipelineSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'repo_url', 'jenkinsfile_path', 'owner_id', 'createAt', 'updateAt', 'deleteAt')
+        fields = ('id', 'pipeline_name', 'repo_url', 'jenkinsfile_path', 'owner_id', 'createAt', 'updateAt', 'deleteAt')
 
 pipeline_schema = PipelineSchema()
 pipelines_schema = PipelineSchema(many=True)
