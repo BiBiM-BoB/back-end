9ef8d8ccbd8c79ffd80782f8ccdd523d3a746c3d
Ubuntu <nebulayoon4700@gmail.com>
2022-09-26 16:33:43 +0000

    feat: security_result db 기능 변경

--- app/models/security_result.py
+++ app/models/security_result.py
@@ -9,6 +9,7 @@ class SecurityResult(db.Model):
     pipeline_id = db.Column(db.Integer, db.ForeignKey('pipelines.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) # 실행시킨 사람
     resultfile_path = db.Column(db.String(1024, 'utf8mb4_unicode_ci'), nullable=False)
+    step = db.Column(db.String(10), nullable=False)
     high = db.Column(db.Integer)
     middle = db.Column(db.Integer)
     low = db.Column(db.Integer)
@@ -16,17 +17,18 @@ class SecurityResult(db.Model):
     updateAt = db.Column(db.DateTime, default=datetime.utcnow())
     deleteAt = db.Column(db.DateTime)
 
-    def __init__(self, pipeline_id, user_id, resultfile_path, high, middle, low):
+    def __init__(self, pipeline_id, user_id, resultfile_path, step, high, middle, low):
         self.pipeline_id = pipeline_id
         self.user_id = user_id
         self.resultfile_path = resultfile_path
+        self.step = step
         self.high = high
         self.middle = middle
         self.low = low
 
 class SecurityResultSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'pipeline_id', 'user_id', 'resultfile_path', 'high', 'middle', 'low', 'createAt', 'updateAt', 'deleteAt')
+        fields = ('id', 'pipeline_id', 'user_id', 'resultfile_path', 'step', 'high', 'middle', 'low', 'createAt', 'updateAt', 'deleteAt')
 
 security_result_schema = SecurityResultSchema()
 security_results_schema = SecurityResultSchema(many=True)
