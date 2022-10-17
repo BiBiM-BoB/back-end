cc73e948b7c4b3c36f36a3b713c1cf3bafa508fd
OneDayGinger <hsremnant9@gmail.com>
2022-10-17 17:31:46 +0900

    feat: JenkinsfileGenerator.py 기능 보완

--- app/utils/BJW/core/generators/JenkinsfileGenerator.py
+++ app/utils/BJW/core/generators/JenkinsfileGenerator.py
@@ -1,24 +1,4 @@
 """OneDayGinger
-Jenkinsfile Generator
-
-햄들이 완성하신 툴들로 Jenkinsfile을 만들어주는 코드.
-
-
-기능 : BJW/core/resources/tools_components 내부의 잘 짜여진 구조에 맞춰
-아래와 같이 groovy stage를 작성하시면, 별다른 것 할 필요 없이 알아서 툴이 적용됩니다.
-
-
-=====================stage example========================
-# ./tools_components/DAST/ZAP/2.Execute_ZAP.groogy
-
-stage('Execute ZAP') {
-    exec('bibim-zap', 'mkdir /zap/wrk')
-    exec('bibim-zap', 'zap-full-scan.py -t http://112.167.178.26:3000 -J /zap/wrk/report.json')
-}
-==========================================================
-
-일반적으로는 tools_components 내부 groovy의 숫자 순서대로 jenkinsfile이 작성되지만, 커스터마이징이 필요한 경우
-아래의 전역변수 tool_order_exception_에 그 순서를 기입해주세요.
 
 """
 
@@ -27,26 +7,38 @@ import xml.etree.ElementTree as ET
 from .GeneratorBase import GeneratorBase
 import json
 
-tool_order_exception_dict = {
-    'ZAP': [('BUILD/NodeJS', 1), ('BUILD/NodeJS', 2), 'DAST/ZAP', ('BUILD/NodeJS', -1)]
-}
-
-
 component_dir = "/home/bibim/back-end/app/utils/BJW/core/generators/resources/tools_components/"
 # component_dir = os.path.abspath('.') + "/home/bibim/back-end/app/utils/BJW/core/generators/resources/tools_components/"
 
+'''
+# Example of input json
+json:
+{
+    'BUILD': {
+        'NodeJS': 1,
+        'Flask': 0
+    }
+    'DAST': {
+        'ZAP': 1, 
+        'Arachni": 0
+    },
+    'SAST': {
+        'CodeQL': 1
+    },
+    'SCA': {
+        'DependencyCheck': 0, 
+        'Dependabot': 1
+    },
+    'SIS': {
+        'GGShield': 1,
+        'GitLeaks': 1
+    }
+}
 
-def json_to_list(input_json):
-    tool_list = []
+# Example of generated tool_list
+tool_list = ['BUILD/NodeJS', 'DAST/ZAP', 'SAST/CodeQL', 'SCA/Dependabot', 'SIS/GGShield', 'SIS/GitLeaks']
+'''
 
-    input_json = json.loads(input_json)
-
-    for stage in input_json.keys():
-        for tool in input_json[stage].keys():
-            if input_json[stage][tool]:
-                tool_list.append(stage + '/' + tool)
-
-    return tool_list
 
 def get_element_by_parent_list(root, parent_list):
     for parent in parent_list:
@@ -54,34 +46,8 @@ def get_element_by_parent_list(root, parent_list):
     return root
 
 
-def init_tools_xml():
-    root = ET.Element("root")
-
-    for dirname, dirnames, filenames in os.walk(component_dir):
-        # print path to all filenames.
-        if "\\" in dirname:
-            parents = dirname[len(component_dir):].split("\\")
-        else:
-            parents = dirname[len(component_dir):].split('/')
-
-        if len(parents) == 1 and parents[0] == '':
-            for subdirname in dirnames:
-                ET.SubElement(root, subdirname)
-        elif dirnames:
-            temp = get_element_by_parent_list(root, parents)
-            temp.attrib["num"] = str(len(dirnames))
-            for subdirname in dirnames:
-                ET.SubElement(temp, subdirname)
-                # ET.SubElement(parents[-1], subdirname)
-        else:
-            get_element_by_parent_list(root, parents).text = str(len(filenames))
-
-    with open(component_dir + 'tools.xml', "wb") as file:
-        xml = ET.ElementTree(root)
-        xml.write(file, method='html', encoding='utf-8')
-
-
 def order_to_file(order):
+    # 
     if type(order) == str:
         text = ""
         tool = order.split('/')[-1]
@@ -104,33 +70,91 @@ def order_to_file(order):
 
 class JenkinsfileGenerator(GeneratorBase):
     def __init__(self, pipeline_name, input_json):
-        # init tools.xml
-        # init_tools_xml()
         self.pipeline_name = pipeline_name
+        self.input_json = input_json
+
         self.jenkinsfile_path = self.localgitdir + "Jenkinsfiles/" + pipeline_name
-        self.tool_list = json_to_list(input_json)
+
+        self.tool_list = self._json_to_list(self.input_json)
 
         self.generate_jenkinsfile(self.tool_list)
         self.commit("Generated Jenkinsfile {pipeline_name}")
 
+    def _find_stage(self, stage: str, tool_list):
+        ret = []
+        for tool in tool_list:
+            if stage in tool:
+                ret.append(tool)
+        return ret
+    
+    def _json_to_list(self, input_json):
+        tool_list = []
 
-    def generate_jenkinsfile(self, tool_list):
+        input_json = json.loads(input_json)
+
+        for stage in input_json.keys():
+            for tool in input_json[stage].keys():
+                if input_json[stage][tool]:
+                    tool_list.append(stage + '/' + tool)
+
+        return tool_list
+
+    # Let's say,
+    # self._write_stages('DAST/ZAP')
+    def _write_stages(self, tool, part=None):
+        # then this function will return Jenkinsfile components about running ZAP, which is variable 'stages' below.
+        stages = ""
+
+        for dirname, _, files in os.walk(component_dir + tool + '/'):
+            files.sort()
+
+            if not part:
+                for i in range(len(files)):
+                    if int(files[i].split('.')[0]) > 0:
+                        files[i:], files[:i] = files[:i], files[i:]
+                        break
+            
+            elif part=='start':
+                for i in range(len(files)):
+                    if int(files[i].split('.')[0] > 0):
+                        files = files[i:]
+                        break
+            
+            elif part=='stop':
+                for i in range(len(files)):
+                    if int(files[i].split('.')[0] > 0):
+                        files = files[:i]
+                        break
+
+            for file in files:
+                file = open(os.path.join(dirname, file), 'r')
+                text = file.read()
+                if text[:6] == "$bibim":
+                    file.close()
+                    stage_list = self._find_stage(text.split(':')[1])
+                    # if component's content is '$bibim:SCA:start' : self._find_stage will return all sca tool list
+                    # for example,
+                    # stage_list = ['SCA/Dependabot', 'SCA/Dependency_check']
+                    for stage in stage_list:
+                        # if component's content is (for example) '$bibim:BUILD:start' : call this func recursively
+                        stages += self._write_stages(stage, text.split(':')[2])
+                else:
+                    file.close()
+                    stages += text
+
+        return stages
+
+    def _generate_jenkinsfile(self, tool_list):
         jenkinsfile = open(self.jenkinsfile_path, 'w')
-        jenkinsfile.write(order_to_file(('FUNC', 1)))
+        jenkinsfile.write(self._write_stages('FUNC', 'start'))
 
-        for tool_path in tool_list:
-            tool = tool_path.split('/')[-1]
-            if tool not in tool_order_exception_dict.keys():
-                jenkinsfile.write(order_to_file(tool_path))
-            else:
-                order_list = tool_order_exception_dict[tool]
-                for order in order_list:
-                    jenkinsfile.write(order_to_file(order))
+        for tool in tool_list:
+            jenkinsfile.write(self._write_stages(tool))
 
-        jenkinsfile.write(order_to_file(('FUNC', -1)))
+        jenkinsfile.write(self._write_stages('FUNC', 'stop'))
         jenkinsfile.close()
 
-    def post_action(self):
+    def _post_action(self):
         return self.localgitdir, "Jenkinsfiles/" + self.pipeline_name
 
 if __name__ == "__main__":
